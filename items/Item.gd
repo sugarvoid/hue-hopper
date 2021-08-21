fa6f9c18b2325c556c9c162c7a14fcc6c61533c1
hamcola <48969490+SugarVoid@users.noreply.github.com>
2021-08-21 07:57:01 -0500

    Changed coin animation. Resized player collision

--- items/Item.gd
+++ items/Item.gd
@@ -10,12 +10,13 @@ func _physics_process(delta) -> void:
 
 func _on_Item_body_entered(body: Node) -> void:
 	if body.is_in_group("player"):
-		body.add_coin(1)
-		Signals.emit_signal("player_coin_amount_changed", 1)
+		#body.add_coin(1)
+		
 		animated_player.play("picked_up")
 
 func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
 	if anim_name == "picked_up":
+		Signals.emit_signal("player_coin_amount_changed", 1)
 		queue_free()
 
 func _on_VisibilityNotifier2D_screen_exited() -> void:
