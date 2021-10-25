2798bc2f9ed15b9ff74fc400c1a1ba67309869bf
hamcola <48969490+SugarVoid@users.noreply.github.com>
2021-10-25 08:39:54 -0500

    Made custom buttons

--- scenes/StartScreen.gd
+++ scenes/StartScreen.gd
@@ -4,30 +4,28 @@ extends Node2D
 func _ready():
 	PlayerData.init_data()
 
-var GuiHelper = preload("res://utils/gui_helper.gd")
 
-func _on_NewGame_gui_input(event: InputEvent) -> void:
-	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.pressed:
+func _is_left_mouse_click(input: InputEventMouseButton) -> bool:
+	return (input is InputEventMouseButton 
+	and input.button_index == BUTTON_LEFT 
+	and input.pressed)
+
+
+func _on_DemoQuit_gui_input(event):
+	pass # Replace with function body.
+
+
+func _on_BtnSettings_gui_input(event):
+	if _is_left_mouse_click(event):
+		var _x = get_tree().change_scene("res://scenes/GameSettings.tscn")
+
+
+func _on_BtnPlay_gui_input(event):
+	if _is_left_mouse_click(event):
 		$AudioStreamPlayer.play()
 		var _x = get_tree().change_scene("res://scenes/Game.tscn")
 
-func _on_Quit_gui_input(event: InputEvent) -> void:
-	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.pressed:
+
+func _on_BtnQuit_gui_input(event):
+	if _is_left_mouse_click(event):
 		get_tree().quit()
-
-func _on_NewGame_mouse_entered():
-	GuiHelper.change_rect_color($NewGame, GuiHelper.rubyRed)
-
-func _on_NewGame_mouse_exited():
-	GuiHelper.change_rect_color($NewGame, GuiHelper.white)
-
-func _on_Quit_mouse_entered():
-	GuiHelper.change_rect_color($Quit, GuiHelper.rubyRed)
-
-func _on_Quit_mouse_exited():
-	GuiHelper.change_rect_color($Quit, GuiHelper.white)
-
-
-func _on_Settings_gui_input(event):
-	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.pressed:
-		var _x = get_tree().change_scene("res://scenes/GameSettings.tscn")
