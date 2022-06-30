fb88d674ecc25d90611e2c8b014ea1acfd80c825
sugarvoid <email>
2022-06-29 20:41:29 -0500

    Changed player sprite

--- scenes/ColorSign.gd
+++ scenes/ColorSign.gd
@@ -1,14 +1,16 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
+onready var sprite = get_node("Sprite")
 # var b = "text"
+export var color : int 
 
 
-# Called when the node enters the scene tree for the first time.
+func set_color(number: int) -> void:
+	self.sprite = number
+
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 
